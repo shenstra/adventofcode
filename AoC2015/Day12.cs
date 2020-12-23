026615970e142637bf2f9d24b3418a43a752f641
Sjoerd Henstra <shenstra@gmail.com>
2020-12-24 00:33:07 +0100

    Add day 12 of 2015

---
+++ AoC2015/Day12.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace Advent.AoC2015
+{
+    internal class Day12
+    {
+        public void Part1()
+        {
+            string line = Input.GetSingleLine(2015, 12);
+            var doc = JsonDocument.Parse(line);
+            Console.WriteLine(SumNumbers(doc.RootElement, (e) => false));
+        }
+
+        public void Part2()
+        {
+            string line = Input.GetSingleLine(2015, 12);
+            var doc = JsonDocument.Parse(line);
+            Console.WriteLine(SumNumbers(doc.RootElement, HasRedProperty));
+        }
+
+        private long SumNumbers(JsonElement element, Func<JsonElement, bool> shouldSkipObject)
+        {
+            return element.ValueKind switch
+            {
+                JsonValueKind.Undefined => 0,
+                JsonValueKind.Object => shouldSkipObject(element) ? 0 : element.EnumerateObject().Sum(p => SumNumbers(p.Value, shouldSkipObject)),
+                JsonValueKind.Array => element.EnumerateArray().Sum(e => SumNumbers(e, shouldSkipObject)),
+                JsonValueKind.String => long.TryParse(element.GetString(), out long l) ? l : 0,
+                JsonValueKind.Number => element.GetInt64(),
+                JsonValueKind.True => 0,
+                JsonValueKind.False => 0,
+                JsonValueKind.Null => 0,
+                _ => 0
+            };
+        }
+
+        private static bool HasRedProperty(JsonElement element)
+        {
+            return element.EnumerateObject().Any(p => p.Value.ValueKind == JsonValueKind.String && p.Value.GetString() == "red");
+        }
+    }
+}
