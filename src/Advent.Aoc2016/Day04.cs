859a168a0119e7bda8a98c559b29c1761d7cf7d7
Sjoerd Henstra <shenstra@gmail.com>
2021-01-20 22:20:28 +0100

    Add day 4 of 2016

---
+++ src/Advent.Aoc2016/Day04.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Advent.Util;
+
+namespace Advent.Aoc2016
+{
+    public class Day04
+    {
+        private readonly Regex roomRegex = new Regex(@"([a-z-]+)-(\d+)\[([a-z]+)\]");
+
+        public void Part1()
+        {
+            var rooms = Input.GetLines(2016, 4).Select(ParseRoomCode);
+            var realRooms = rooms.Where(r => GetChecksum(r.name) == r.checksum);
+            Console.WriteLine(realRooms.Sum(r => r.sectorId));
+        }
+
+        public void Part2()
+        {
+            var rooms = Input.GetLines(2016, 4).Select(ParseRoomCode);
+            var realRooms = rooms.Where(r => GetChecksum(r.name) == r.checksum);
+            var northyRooms = realRooms.Where(r => DecryptRoomName(r.name, r.sectorId).Contains("north"));
+            foreach (var (name, sectorId, checksum) in northyRooms)
+            {
+                Console.WriteLine($"{DecryptRoomName(name, sectorId)}: {sectorId}");
+            }
+        }
+
+        private (string name, int sectorId, string checksum) ParseRoomCode(string input)
+        {
+            var groups = roomRegex.Match(input).Groups;
+            return (groups[1].Value, int.Parse(groups[2].Value), groups[3].Value);
+        }
+
+        private static string GetChecksum(string name)
+        {
+            return string.Concat(name.Replace("-", "")
+                .GroupBy(c => c)
+                .OrderByDescending(g => g.Count())
+                .ThenBy(g => g.Key)
+                .Select(g => g.Key)
+                .Take(5));
+        }
+
+        private static string DecryptRoomName(string name, int sectorId)
+        {
+            return string.Concat(name.Select(c => c switch
+            {
+                '-' => ' ',
+                _ => (char)('a' + ((c - 'a' + sectorId) % 26)),
+            }));
+        }
+    }
+}
