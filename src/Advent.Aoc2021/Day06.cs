8e5628d55a41a0f12acb9cc1148370ae6b413c96
Sjoerd Henstra <shenstra@gmail.com>
2021-12-06 08:45:41 +0100

    Add day 6 of 2021

---
+++ src/Advent.Aoc2021/Day06.cs
@@ -0,0 +1,51 @@
+﻿using Advent.Util;
+
+namespace Advent.Aoc2021
+{
+    public class Day06
+    {
+        private const int initialTimer = 8;
+
+        public void Part1()
+        {
+            string line = Input.GetSingleLine(2021, 6);
+            long[] fishWithTimer = InitializeTimerCounts(line);
+            RunSimulation(fishWithTimer, 80);
+            Console.WriteLine(fishWithTimer.Sum());
+        }
+
+        public void Part2()
+        {
+            string line = Input.GetSingleLine(2021, 6);
+            long[] fishWithTimer = InitializeTimerCounts(line);
+            RunSimulation(fishWithTimer, 256);
+            Console.WriteLine(fishWithTimer.Sum());
+        }
+
+        private static long[] InitializeTimerCounts(string input)
+        {
+            int[] fishTimers = input.Split(',').Select(int.Parse).ToArray();
+            long[] fishWithTimer = new long[initialTimer + 1];
+            foreach (int fishTimer in fishTimers)
+            {
+                fishWithTimer[fishTimer]++;
+            }
+
+            return fishWithTimer;
+        }
+
+        private static void RunSimulation(long[] fishWithTimer, int days)
+        {
+            for (int day = 0; day < days; day++)
+            {
+                long mommies = fishWithTimer[0];
+                for (int timer = 0; timer < initialTimer; timer++)
+                {
+                    fishWithTimer[timer] = fishWithTimer[timer + 1];
+                }
+                fishWithTimer[8] = mommies;
+                fishWithTimer[6] += mommies;
+            }
+        }
+    }
+}
