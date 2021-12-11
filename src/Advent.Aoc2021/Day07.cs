7250e62fe71beddd0bd6963e8d17ad7b33aac798
Sjoerd Henstra <shenstra@gmail.com>
2021-12-11 16:20:37 +0100

    Optimize day 7 of 2021
    Triangular numbers can be calculated much more easily

--- src/Advent.Aoc2021/Day07.cs
+++ src/Advent.Aoc2021/Day07.cs
@@ -4,8 +4,6 @@ namespace Advent.Aoc2021
 {
     public class Day07
     {
-        private readonly Dictionary<int, int> triangularNumbers = new() { { 0, 1 }, { 1, 1 } };
-
         public void Part1()
         {
             int[] crabPositions = Input.GetSingleLine(2021, 7).SplitToInts();
@@ -25,18 +23,10 @@ namespace Advent.Aoc2021
                 .Min(target => crabPositions.Sum(position => fuelCostFunction(position, target)));
         }
 
-        private int LinearFuelCost(int a, int b) => Math.Abs(a - b);
+        private static int LinearFuelCost(int a, int b) => Math.Abs(a - b);
 
-        private int TriangularFuelCost(int a, int b) => TriangularNumber(Math.Abs(a - b));
+        private static int TriangularFuelCost(int a, int b) => TriangularNumber(Math.Abs(a - b));
 
-        private int TriangularNumber(int n)
-        {
-            if (!triangularNumbers.ContainsKey(n))
-            {
-                triangularNumbers[n] = TriangularNumber(n - 1) + n;
-            }
-
-            return triangularNumbers[n];
-        }
+        private static int TriangularNumber(int n) => n * (n + 1) / 2;
     }
 }
