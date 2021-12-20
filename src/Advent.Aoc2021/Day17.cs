4018d2704a2033e165435e387d0f7b2e7ece8600
Sjoerd Henstra <shenstra@gmail.com>
2021-12-20 16:31:55 +0100

    Add day 17 of 2021

---
+++ src/Advent.Aoc2021/Day17.cs
@@ -0,0 +1,102 @@
+﻿using System.Text.RegularExpressions;
+using Advent.Util;
+
+namespace Advent.Aoc2021
+{
+    public class Day17
+    {
+        private readonly Regex targetRegex = new(@"target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)");
+        private const int MaxDeltaY = 1000;
+
+        public void Part1()
+        {
+            var target = ParseTarget(Input.GetSingleLine(2021, 17));
+            Console.WriteLine(FindHighPoints(target).Max());
+        }
+
+        public void Part2()
+        {
+            var target = ParseTarget(Input.GetSingleLine(2021, 17));
+            Console.WriteLine(FindHighPoints(target).Count());
+        }
+
+        private static IEnumerable<int> FindHighPoints(Target target)
+        {
+            uint minDeltaX = (uint)Math.Sqrt(target.MinX * 2);
+            for (uint deltaX = minDeltaX; deltaX <= target.MaxX; deltaX++)
+            {
+                for (int deltaY = target.MinY; deltaY < MaxDeltaY; deltaY++)
+                {
+                    if (HitsTarget(new(deltaX, deltaY), target, out int maxY))
+                    {
+                        yield return maxY;
+                    }
+                }
+            }
+        }
+
+        private static bool HitsTarget(Velocity velocity, Target target, out int maxY)
+        {
+            var position = new Position(0, 0);
+            maxY = 0;
+
+            while (true)
+            {
+                if (IsInTarget(position, target))
+                {
+                    return true;
+                }
+
+                if (!StillOnTarget(position, target, velocity))
+                {
+                    return false;
+                }
+
+                (position, velocity) = AdvanceProjectile(position, velocity);
+                maxY = Math.Max(maxY, position.Y);
+            }
+        }
+
+        private static (Position, Velocity) AdvanceProjectile(Position position, Velocity velocity)
+        {
+            return (new Position(position.X + velocity.X, position.Y + velocity.Y),
+                new Velocity(velocity.X > 0 ? velocity.X - 1 : 0, velocity.Y - 1));
+        }
+
+        private static bool IsInTarget(Position position, Target target)
+        {
+            return position.X >= target.MinX &&
+                position.X <= target.MaxX &&
+                position.Y >= target.MinY &&
+                position.Y <= target.MaxY;
+        }
+
+        private static bool StillOnTarget(Position current, Target target, Velocity velocity)
+        {
+            if (velocity.X == 0)
+            {
+                return current.X >= target.MinX && current.X <= target.MaxX && current.Y >= target.MinY;
+            }
+            else if (current.X > target.MaxX)
+            {
+                return false;
+            }
+
+            return velocity.Y > 0 || current.Y > target.MinY;
+        }
+
+        private Target ParseTarget(string input)
+        {
+            var groups = targetRegex.Match(input).Groups;
+            return new Target(
+                int.Parse(groups[1].Value),
+                int.Parse(groups[2].Value),
+                int.Parse(groups[3].Value),
+                int.Parse(groups[4].Value));
+        }
+
+        internal record struct Target(int MinX, int MaxX, int MinY, int MaxY);
+        internal record struct Position(uint X, int Y);
+        internal record struct Velocity(uint X, int Y);
+    }
+}
