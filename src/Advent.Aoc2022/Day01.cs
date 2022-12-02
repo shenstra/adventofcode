4f5b62a2fb38bd7d4b8dcf6b3d3fe50c7ea8ed32
Sjoerd Henstra <shenstra@gmail.com>
2022-12-02 23:16:42 +0100

    Add day 1 of 2022

---
+++ src/Advent.Aoc2022/Day01.cs
@@ -0,0 +1,41 @@
+﻿using Advent.Util;
+
+namespace Advent.Aoc2022
+{
+    public class Day01
+    {
+        public void Part1()
+        {
+            var lines = Input.GetLines(2022, 1).ToList();
+            var inventories = ParseInventories(lines);
+            Console.WriteLine(inventories.Max());
+        }
+
+        public void Part2()
+        {
+            var lines = Input.GetLines(2022, 1).ToList();
+            var inventories = ParseInventories(lines);
+            var topThree = inventories.OrderByDescending(i => i).Take(3);
+            Console.WriteLine(topThree.Sum());
+        }
+
+        private IEnumerable<int> ParseInventories(List<string> lines)
+        {
+            int inventory = 0;
+            foreach (string line in lines)
+            {
+                if (line == string.Empty)
+                {
+                    yield return inventory;
+                    inventory = 0;
+                }
+                else
+                {
+                    inventory += int.Parse(line);
+                }
+            }
+
+            yield return inventory;
+        }
+    }
+}
--- src/Advent/Program.cs
+++ src/Advent/Program.cs
@@ -11,8 +11,8 @@ namespace Advent
 
             try
             {
-                new Aoc2021.Day21().Part1();
-                new Aoc2021.Day21().Part2();
+                new Aoc2022.Day01().Part1();
+                new Aoc2022.Day01().Part2();
             }
             catch (Exception ex)
             {
